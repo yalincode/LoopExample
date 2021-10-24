0a6b193a93e2deec7a9cae8149025e307c2d1e3b
Yalın Sonat <yalin_sonat@hotmail.com>
2021-10-24 10:22:59 +0300

    Add project files.

---
+++ LoopExample/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿
+namespace LoopExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnBasla = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnLevelArttir = new System.Windows.Forms.Button();
+            this.btnLevelAzalt = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(0, 71);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(946, 519);
+            this.flowLayoutPanel1.TabIndex = 0;
+            // 
+            // btnBasla
+            // 
+            this.btnBasla.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnBasla.Location = new System.Drawing.Point(12, 12);
+            this.btnBasla.Name = "btnBasla";
+            this.btnBasla.Size = new System.Drawing.Size(110, 38);
+            this.btnBasla.TabIndex = 0;
+            this.btnBasla.Text = "Başla";
+            this.btnBasla.UseVisualStyleBackColor = true;
+            this.btnBasla.Click += new System.EventHandler(this.btnBasla_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.btnLevelAzalt);
+            this.panel1.Controls.Add(this.btnLevelArttir);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnBasla);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(946, 65);
+            this.panel1.TabIndex = 1;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(853, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 24);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Puan: 0";
+            // 
+            // btnLevelArttir
+            // 
+            this.btnLevelArttir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLevelArttir.Location = new System.Drawing.Point(627, 12);
+            this.btnLevelArttir.Name = "btnLevelArttir";
+            this.btnLevelArttir.Size = new System.Drawing.Size(105, 30);
+            this.btnLevelArttir.TabIndex = 2;
+            this.btnLevelArttir.Text = "Level Arttır";
+            this.btnLevelArttir.UseVisualStyleBackColor = true;
+            this.btnLevelArttir.Click += new System.EventHandler(this.btnLevelArttir_Click);
+            // 
+            // btnLevelAzalt
+            // 
+            this.btnLevelAzalt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnLevelAzalt.Location = new System.Drawing.Point(738, 12);
+            this.btnLevelAzalt.Name = "btnLevelAzalt";
+            this.btnLevelAzalt.Size = new System.Drawing.Size(109, 30);
+            this.btnLevelAzalt.TabIndex = 3;
+            this.btnLevelAzalt.Text = "Level Azalt";
+            this.btnLevelAzalt.UseVisualStyleBackColor = true;
+            this.btnLevelAzalt.Click += new System.EventHandler(this.btnLevelAzalt_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(338, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 24);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Level: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 590);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btnBasla;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnLevelAzalt;
+        private System.Windows.Forms.Button btnLevelArttir;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ LoopExample/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoopExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < 100; i++)
+            {
+                Button button = new Button();
+                button.Name = "btn1"+i.ToString();
+                button.Width = 50;
+                button.Height = 50;
+                button.Text = i.ToString();
+                button.Click += Button_Click;//+= tab tab yaparak click eventi oluşturulur.
+                flowLayoutPanel1.Controls.Add(button);
+            }
+            label2.Text = $"Level: {timer1.Interval}";
+
+
+        }
+        int puan = 0;
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;//Tıkladığımız button senderdan buraya gelir.
+            if (button.BackColor == Color.Red)
+            {
+                puan += 10;
+                
+            }
+            else
+            {
+                puan -= 5;
+                
+            }
+            label1.Text = $"Puan: {puan}";
+        }
+
+        Random rnd = new Random();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int rastgeleSayi = rnd.Next(0, 101);
+            for (int i = 0; i < flowLayoutPanel1.Controls.Count; i++)
+            {
+                Control control = flowLayoutPanel1.Controls[i];
+                if (control is Button)
+                {
+                    Button button = control as Button;
+                    int buttonNumber = Convert.ToInt32(button.Text);
+                    if (rastgeleSayi==buttonNumber)
+                    {
+                        button.BackColor = Color.Red;
+                        button.ForeColor = Color.Yellow;
+                    }
+                    else
+                    {
+                        button.BackColor = Color.White;
+                        button.ForeColor = Color.Black;
+                    }
+
+                }
+            }
+        }
+
+        private void btnBasla_Click(object sender, EventArgs e)
+        {
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            else
+            {
+                timer1.Start();
+            }
+        }
+
+        private void btnLevelArttir_Click(object sender, EventArgs e)
+        {
+            
+            timer1.Interval = timer1.Interval - 100;
+            label2.Text = $"Level: {timer1.Interval}";
+            
+        }
+
+        private void btnLevelAzalt_Click(object sender, EventArgs e)
+        {
+            timer1.Interval = timer1.Interval + 100;
+            label2.Text = $"Level: {timer1.Interval}";
+        }
+    }
+}
